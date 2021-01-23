70c9dd910e520b121af92ee0a67fe0427bfc7329
JoeRedskin <36441911+JoeRedskin@users.noreply.github.com>
2021-01-23 11:10:18 +0300

    clean code

--- app/src/main/java/com/example/weatherapp/CityAdapter.kt
+++ app/src/main/java/com/example/weatherapp/CityAdapter.kt
@@ -4,26 +4,30 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
-import com.example.weatherapp.CityAdapter.MyViewHolder
+import com.example.weatherapp.CityAdapter.CityViewHolder
 import com.example.weatherapp.databinding.CityItemBinding
 
 
-class CityAdapter(private val citiesList: List<City>) : RecyclerView.Adapter<MyViewHolder>() {
+class CityAdapter(private val citiesList: List<City>) : RecyclerView.Adapter<CityViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CityViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         val binding = DataBindingUtil.inflate<CityItemBinding>(inflater, R.layout.city_item, parent, false)
-        return MyViewHolder(binding)
+        return CityViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val city = citiesList[position]
-        holder.binding.city = city
+    override fun onBindViewHolder(holder: CityViewHolder, position: Int) {
+        holder.bind(citiesList[position])
     }
 
     override fun getItemCount(): Int {
         return citiesList.size
     }
 
-    class MyViewHolder(val binding: CityItemBinding) : RecyclerView.ViewHolder(binding.root)
+    class CityViewHolder(private val binding: CityItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(city: City) {
+            binding.city = city
+            binding.executePendingBindings()
+        }
+    }
 }
