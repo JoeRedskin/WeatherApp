4e3f0a7a3bbfbf950ca8605d4b912b02a9808fba
JoeRedskin <36441911+JoeRedskin@users.noreply.github.com>
2021-01-29 15:06:15 +0300

    clean code

--- app/src/main/java/com/example/weatherapp/fragment/CityDetailsFragment.kt
+++ app/src/main/java/com/example/weatherapp/fragment/CityDetailsFragment.kt
@@ -1,40 +1,28 @@
 package com.example.weatherapp.fragment
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.example.weatherapp.R
-import com.example.weatherapp.api.CityServices
-import com.example.weatherapp.database.CityDao
 import com.example.weatherapp.databinding.FragmentCityDetailsBinding
 import com.example.weatherapp.model.CityDetailsViewModel
-import com.example.weatherapp.model.CityRepository
-import org.koin.android.ext.android.inject
+import org.koin.android.viewmodel.ext.android.viewModel
+import org.koin.core.parameter.parametersOf
 
 class CityDetailsFragment : Fragment() {
 
     private lateinit var binding: FragmentCityDetailsBinding
-
-    private val cityDao: CityDao by inject()
-    private val cityService: CityServices by inject()
     private val args: CityDetailsFragmentArgs by navArgs()
 
-    //    private val viewModel by viewModel<CityDetailsViewModel> {
-//        parametersOf(args.id)
-//    }
-
-    private val viewModel: CityDetailsViewModel by viewModels(
-            factoryProducer = { CityDetailViewModelFactory(args.id, CityRepository(cityService, cityDao)) }
-    )
+    private val viewModel by viewModel<CityDetailsViewModel> {
+        parametersOf(args.id)
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
